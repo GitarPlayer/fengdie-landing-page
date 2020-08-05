43254c90e8d403e7c480856be0c50f6a354a7474
penguin <christian_bucheli@hotmail.com>
2020-08-05 15:36:47 +0200

    need_to_fix_ipad

--- src/Home/Page1.jsx
+++ src/Home/Page1.jsx
@@ -22,7 +22,7 @@ export default function Page1() {
     <div className="home-layout-wrapper home-func-wrapper" id="home-func" >
       <h2>My services</h2>
       <i className="line" />
-      <OverPack className="home-layout" location="home-func" playScale={0.4}>
+      <OverPack className="home-layout" id="references" location=" home-func" playScale={0.4}>
         <QueueAnim className="home-func" type="bottom" key="home-func" ease="easeOutQuart" leaveReverse>
           <QueueAnim
             key="content"
@@ -34,5 +34,5 @@ export default function Page1() {
           </QueueAnim>
         </QueueAnim>
       </OverPack>
-    </div>);
+    </div >);
 }
